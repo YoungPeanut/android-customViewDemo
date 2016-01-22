cfa4923e7241feb48a2ed07e7dc3ddecaf5e0f9c
young <showarm@sina.com>
2016-01-22 08:35:24 +0800

    minionsview

---
+++ app/src/main/java/info/ipeanut/customviewdemo/view/minionsview/MinionsView.java
@@ -0,0 +1,23 @@
+package info.ipeanut.customviewdemo.view.minionsview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * http://www.tuicool.com/articles/EBfu2m2
+ * Created by chenshao on 16/1/22.
+ */
+public class MinionsView extends View {
+    public MinionsView(Context context) {
+        super(context);
+    }
+
+    public MinionsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MinionsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
